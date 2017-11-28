730599c2141928123680ebe5047e5b6d8c041875
sanouo <sanouo591@gmail.com>
2017-11-28 10:46:43 +0100

    insertion base mvc

--- views/template/header.php
+++ views/template/header.php
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html class="no-js" lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Your site title</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Library</title>
+    <meta name="description" content="Creation of an application of management of vehicle">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
   </head>
+
   <body>
-    <header>
-      <h1>Main title in the header</h1>
-    </header>
