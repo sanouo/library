3a79efb065e00be8f9cfa60a924fa07b01520ab7
sanouo <sanouo591@gmail.com>
2017-12-01 10:26:11 +0100

    create function select for show user in the page user.php

--- views/userView.php
+++ views/userView.php
@@ -4,7 +4,7 @@
 
 
   <!-- start of the div class block -->
- <div class="block">
+ <!-- <div class="block">
 
    <?php foreach ($info as $key => $value){
      ?>
@@ -14,7 +14,7 @@
                 <div class="card col-12 col-md-12 col-lg-12">
                   <div class="card-block col-12 col-md-12 col-lg-12">
                   <h3 class="card-title"><?php echo $value->getName(); ?></h3>
-                  <p class="card-text"><?php echo $value->getAdress(); ?></p>
+                  <p class="card-text"><?php echo $value->getAddress(); ?></p>
                   <p class="card-text"><?php echo $value->getCity(); ?></p>
                   </div>
                 </div>
@@ -25,8 +25,35 @@
               }
             ?>
 
-  </div>
+  </div> -->
     <!-- end of the div class block -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>address</th>
+          <th>City</th>
+        </tr>
+      </thead>
+
+    <?php foreach ($info as $key => $value){
+      ?>
+
+      <tbody>
+        <tr>
+          <td><?php echo $value->getName(); ?></td>
+          <td><?php echo $value->getAddress(); ?></td>
+          <td><?php echo $value->getCity(); ?></td>
+        </tr>
+
+      </tbody>
+
+
+    <?php
+      }
+    ?>
+
+  </table>
 
  <?php
    require("template/footer.php");
