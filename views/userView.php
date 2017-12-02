95dbeaca0b98fd115465de01bf62e5334de6389a
sanouo <sanouo591@gmail.com>
2017-12-02 08:42:44 +0100

    add input available

--- views/userView.php
+++ views/userView.php
@@ -3,30 +3,7 @@
  ?>
 
 
-  <!-- start of the div class block -->
- <!-- <div class="block">
-
-   <?php foreach ($info as $key => $value){
-     ?>
-
-     <div class="container accounts col-12 col-md-6 col-lg-4">
-       <div class="row">
-                <div class="card col-12 col-md-12 col-lg-12">
-                  <div class="card-block col-12 col-md-12 col-lg-12">
-                  <h3 class="card-title"><?php echo $value->getName(); ?></h3>
-                  <p class="card-text"><?php echo $value->getAddress(); ?></p>
-                  <p class="card-text"><?php echo $value->getCity(); ?></p>
-                  </div>
-                </div>
-        </div>
-    </div>
-
-            <?php
-              }
-            ?>
-
-  </div> -->
-    <!-- end of the div class block -->
+  
     <table class="table">
       <thead>
         <tr>
@@ -54,6 +31,8 @@
     ?>
 
   </table>
+
+  <a href="index.php"><button type="button"  class="btn btn-primary">Home</button></a>
 
  <?php
    require("template/footer.php");
